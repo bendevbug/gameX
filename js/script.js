6986ff2f74632ff8878d013bc77fd0907d6575f0
Maysa <maysaraquel.ia@gmail.com>
2022-04-05 22:02:35 -0300

    Feito página login e inputs, resta botão de login

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ function menu() {
     }
 
 }
+
+function mudaBg() {
+    var v = document.getElementById("myInput");
+    v.style.background = "#fff";
+    var b = document.getElementById("myInputs");
+    b.style.background = "#fff";
+    console.log('oi');
+}
+
+
+function mudaBg2() {
+    var x = document.getElementById("myInput");
+    x.style.background = "#CE99F0";
+    var z = document.getElementById("myInputs");
+    z.style.background = "#CE99F0";
+    console.log('oi');
+}
